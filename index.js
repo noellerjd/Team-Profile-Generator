6ea80f9aa14418668fefbcd0fed74d444861224e
noellerjd <noellerninja@gmail.com>
2022-07-04 17:54:19 -0600

    html_base parsed

--- index.js
+++ index.js
@@ -1,8 +1,96 @@
 const inquirer = rquire ('inquirer');
 const fs = require('fs');
 
-const html_base =({}) => `
+const html_base =({project_title, member1_name, member1_jobtitle, member1_ID, member1_email, member1_github, member2_name, member2_jobtitle, member2_ID, member2_email, member2_github, member3_name, member3_jobtitle, member3_ID, member3_email, member3_github, member4_name, member4_jobtitle, member4_ID, member4_email, member4_github, member5_name, member5_jobtitle, member5_ID, member5_email, member5_github}) => `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Generated Team Profile</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/style.css">
+</head>
+<body>
+    <section class="nav">
+        <div class="header">
+            <h1 class="teamname">${project_title}</h1>
+        </div>
+    </section>
+    <section class="body">
+        <div class="cardcontainer">
+            <div class="card">
+                <div class="cardheader">
+                    <h1>${member1_name}</h1>
+                    <h3>${member1_jobtitle}</h3>
+                </div>
+                <div class="cardbody">
+                    <div class="tile"><p>ID: ${member1_ID}</p></div>
+                    <div class="tile">Email: <a href="mailto:${member1_email}">${member1_email}</a></div>
+                    <div class="tile">Github: <a href="https://github.com/${member1_github}">${member1_github}</a></div>
+                </div>
+            </div>
+        </div>
+        <div class="cardcontainer">
+            <div class="card">
+                <div class="cardheader">
+                    <h1>${member2_name}</h1>
+                    <h3>${member2_jobtitle}</h3>
+                </div>
+                <div class="cardbody">
+                    <div class="tile"><p>ID: ${member2_ID}</p></div>
+                    <div class="tile">Email: <a href="mailto:${member2_email}">${member2_email}</a></div>
+                    <div class="tile">Github: <a href="https://github.com/${member2_github}">${member2_github}</a></div>
+                </div>
+            </div>
+        </div>
+        <div class="cardcontainer">
+            <div class="card">
+                <div class="cardheader">
+                    <h1>${member3_name}</h1>
+                    <h3>${member3_jobtitle}</h3>
+                </div>
+                <div class="cardbody">
+                    <div class="tile"><p>ID: ${member3_ID}</p></div>
+                    <div class="tile">Email: <a href="mailto:${member3_email}">${member3_email}</a></div>
+                    <div class="tile">Github: <a href="https://github.com/${member3_github}">${member3_github}</a></div>
+                </div>
+            </div>
+        </div>
+        <div class="cardcontainer">
+            <div class="card">
+                <div class="cardheader">
+                    <h1>${member4_name}</h1>
+                    <h3>${member4_jobtitle}</h3>
+                </div>
+                <div class="cardbody">
+                    <div class="tile"><p>ID: ${member4_ID}</p></div>
+                    <div class="tile">Email: <a href="mailto:${member4_email}">${member4_email}</a></div>
+                    <div class="tile">Github: <a href="https://github.com/${member4_github}">${member4_github}</a></div>
+                </div>
+            </div>
+        </div>
+        <div class="cardcontainer">
+            <div class="card">
+                <div class="cardheader">
+                    <h1>${member5_name}</h1>
+                    <h3>${member5_jobtitle}</h3>
+                </div>
+                <div class="cardbody">
+                    <div class="tile"><p>ID: ${member5_ID}</p></div>
+                    <div class="tile">Email: <a href="mailto:${member5_email}">${member5_email}</a></div>
+                    <div class="tile">Github: <a href="https://github.com/${member5_github}">${member5_github}</a></div>
+                </div>
+            </div>
+        </div>
+    </section>
 
+    
+</body>
+</html>
 `
 
 init = () => {
